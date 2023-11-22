22d0c3cfd3704e656df41ac48019235ce700f7e5
liuxb <569973364@qq.com>
2023-11-22 10:16:39 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+header("content-type:text/html;charset=utf-8");  //设置编码
+ ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Welcome</title>
+	</head>
+	<body>
+	<h1>如果光都不能逃脱我的掌控，那么你还有什么希望呢</h1>
+	<hr/>
+	<p>谜团O(∩_∩)O</p>
+	
+	<script type="text/javascript">
+
+		//禁用右键
+		window.document.oncontextmenu = function(){ 
+
+		return false;
+
+		} 
+	</script>
+	</body>
+</html>
